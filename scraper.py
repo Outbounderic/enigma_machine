0dcb2ad4c36d68875556e5036f46f701bdf134e2
Eric Van Meter <ericvanmeter57@gmail.com>
2025-05-21 18:24:00 -0400

    Web Scraper: Finished web scraping tool.

--- scraper.py
+++ scraper.py
@@ -1,12 +1,34 @@
 import requests
 from bs4 import BeautifulSoup
-import json
 
-def scrape():
-  url = "https://bykea.com/"
-  response = requests.get(url)
-  soup = BeautifulSoup(response.text, 'html.parser')
-  print(soup)
+target_url = input("Enter target URL: \n")
+print(f'Attempting to get metadata from: {target_url}')
+
+try:
+  res = requests.get(target_url)
+  res.raise_for_status()
+  print("Successfully fetched the page!")
+  soup = BeautifulSoup(res.content, 'html.parser')
+  print("\n--- HTML Meta Tags found ---")
+  meta_tags = soup.find_all('meta')
   
-if __name__ == '__main__':
-  scrape()
+  if not meta_tags:
+    print("No <meta> tags found on this page.")
+  else:
+    for i, tag in enumerate(meta_tags):
+      print(f'\nMeta Tag #{i+1}:')
+      if tag.get('charset'):
+        print(f' Charset: {tag.get("charset")}')
+      if tag.get('name'):
+        print(f' Name: {tag.get("name")}')
+      if tag.get('property'):
+        print(f' Property: {tag.get("property")}')
+      if tag.get('content'):
+        print(f' Content: {tag.get("content")}')
+      # print(f' Full tag: {tag}')
+except requests.exceptions.RequestException as e:
+  print(f'Could not get the URL. Error {e}')
+except Exception as e:
+  print(f'An unexpected error occurred: {e}')
+  
+print("\n--- End of Script ---")
